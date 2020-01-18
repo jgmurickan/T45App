3751334dc122c53c894817ed68c7fbbd961b8789
Jobin Murickan <jobinmurickan@Jobins-MacBook-Pro.local>
2020-01-17 20:52:34 -0600

    working whiteboard

--- screens/WhiteBoardScreen.js
+++ screens/WhiteBoardScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {ScrollView, View, ViewPropTypes, Text, Picker, StyleSheet, Button, Linking, Alert, Dimensions} from 'react-native';
+import {ScrollView, View, ViewPropTypes, Text, Picker, StyleSheet, Button, Linking, Alert, Dimensions, Platform} from 'react-native';
 import PropTypes from 'prop-types';
 import * as WebBrowser from 'expo-web-browser';
 import Constants from 'expo-constants';
@@ -87,13 +87,31 @@ class Strike extends Component {
   
   render() {
     const pg = this.props.navigation.getParam('page', '')
-    return (
-      <PDFReader
-        source={{
-          uri: 'https://cnatra.navy.mil/local/docs/instructions/1542.167.pdf#page' + pg,
-        }}
-      />
-    )
+    console.log(pg)
+    if(pg == '163') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/gysrfujkb2udmy1/BI_STRIKE.pdf?dl=0-R'  }} />); }
+    if(pg == '172') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/kqxutg3rt22g2c4/RI_STRIKE.pdf?dl=0' }} />); }
+    if(pg == '87')  { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/tbp3ugaztpjr8i7/FAM_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '99')  { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/ucyduraxii6g84d/OCF41_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '180') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/alemiovreq56b33/AN_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '186') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/gvie7ev0dg6f1ns/IR_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '218') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/dd0qpl1wn7v9r5g/FRM_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '223') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/gbrnol1jelrrbey/CRUISE_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '228') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/8ev738fwsjlmxv8/DIV_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '110') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/fd7kgnhg2n1dhup/NFM_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '117') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/wiih5id3q34brcm/FCL_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '235') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/0iwq9soaluvqf64/NFR_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '195') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/7ygw7h1uj7owuc3/ADVANCEDAN_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '269') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/c0smkvdltssyhfq/ON_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '275') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/box4z6ehhfjfvj0/RR_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '284') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/b8ffpucmd14h9lv/STK_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '295') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/5a3ck41z1us70x1/TAC_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '305') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/unii9a2inm2biyy/OCF42_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '307') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/kabims1g4c3mg2o/BFM_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '321') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/ts269l8cvzf22n9/SEM_STRIKE.pdf?dl=0' }}/>); }
+    if(pg == '325') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/x9mu5cam9knhrjt/CQ_STRIKE.pdf?dl=0' }}/>); }
+    
+    return (<Text>Unable to display PDF</Text>)
+    
   }
 }
 
@@ -101,15 +119,24 @@ class E2C2 extends Component {
   
   render() {
     const pg = this.props.navigation.getParam('page', '')
-    return (
-      <PDFReader
-        source={{
-          uri: 'https://cnatra.navy.mil/local/docs/instructions/1542.176.pdf#page' + pg,
-        }}
-      />
-    )
+    console.log(pg)
+    if(pg == '175') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/jeom3jdmwpzo57m/BI_E2C2.pdf?dl=0'  }} />); }
+    if(pg == '184') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/t8nchgmhhozapaf/RI_E2C2.pdf?dl=0' }} />); }
+    if(pg == '98')  { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/8l7dc122l8q1f9q/FAM_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '110') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/kus94pfz1d6dp0q/OCF41_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '192') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/k2p64qwx0sko7vi/AN_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '198') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/ms3ref1715dtx2u/IR_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '220') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/dph1ragtfc43fap/FRM_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '227') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/ucbk5itw667tvg5/CRUISE_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '232') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/4psmp7njcl6cfdo/DIV_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '124') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/9k5nj33r0s9owal/NFM_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '132') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/5k44e9k1n1067a7/FCL_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '240') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/yufk6ngsmiqzped/NFR_E2C2.pdf?dl=0' }}/>); }
+    if(pg == '249') { return (<PDFReader source={{ uri: 'https://dl.dropboxusercontent.com/s/zzbvb8sf596em3w/CQ_E2C2.pdf?dl=0' }}/>); }
+    
+    return (<Text>Unable to display PDF</Text>)
+    
   }
-
 }
 
 const RootStack = createStackNavigator(
